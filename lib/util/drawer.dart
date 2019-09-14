6d2144e81b95793a8c77d2b2f10cb0b74922a39d
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-09-14 23:47:43 +0530

    responsive design for all pages on all screen sizes

--- lib/util/drawer.dart
+++ lib/util/drawer.dart
@@ -2,9 +2,11 @@ import '../screens/holidays.dart';
 import 'package:flutter/material.dart';
 import '../screens/todoscreen.dart';
 import '../screens/faculty.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MainDrawer extends StatelessWidget {
   Widget buildListTile(String title, IconData icon, Function handlingTap) {
+
     return ListTile(
       leading: Icon(
         icon,
@@ -24,25 +26,33 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double defaultScreenWidth = 400.0;
+    double defaultScreenHeight = 810.0;
+    ScreenUtil.instance = ScreenUtil(
+      width: defaultScreenWidth,
+      height: defaultScreenHeight,
+      allowFontScaling: true,
+    )..init(context);
+
     return Drawer(
       child: Column(
         children: <Widget>[
           Container(
-            height: 120,
+            height: ScreenUtil.instance.setHeight(130),
             width: double.infinity,
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(ScreenUtil.instance.setWidth(20.0)),
             alignment: Alignment.centerLeft,
             child: Text(
               "Assist",
               style: TextStyle(
                 fontWeight: FontWeight.w900,
-                fontSize: 30,
+                fontSize: ScreenUtil.instance.setSp(30.0),
                 color: Theme.of(context).primaryColor,
               ),
             ),
           ),
           SizedBox(
-            height: 20,
+            height: ScreenUtil.instance.setHeight(20),
           ),
           buildListTile("Home", Icons.home, (){
             Navigator.of(context).pushReplacementNamed('/');
@@ -57,7 +67,7 @@ class MainDrawer extends StatelessWidget {
             Navigator.of(context).pushNamed(HolidayList.routeName);
           }),
           SizedBox(
-            height: 30,
+            height: ScreenUtil.instance.setHeight(30),
           ),
           Container(
             child: Text("Built with ❤️ and ☕ by Nikhil Singh"),
