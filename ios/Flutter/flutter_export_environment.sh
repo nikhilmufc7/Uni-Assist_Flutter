6066afecee5acf94be738ac460a76316f65cf4b1
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-09-16 22:11:03 +0530

    changes versions for play release

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -6,6 +6,6 @@ export "FLUTTER_TARGET=/Users/nikhilsingh/Desktop/Flutter_Project/dypiu_announce
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/nikhilsingh/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=2.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
