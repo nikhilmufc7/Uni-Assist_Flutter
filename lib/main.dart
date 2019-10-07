9dd8064e6a1926bc71b880c0232a19de75d00401
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-10-08 00:36:27 +0530

    added dark theme and ready for version 3.0 release

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,36 @@ import './screens/events.dart';
 import './screens/about.dart';
 import './screens/bca1/referenceBooks1.dart';
 import './screens/bca2/referenceBooks2.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'util/theme_notifier.dart';
+import 'util/theme.dart';
+import 'package:provider/provider.dart';
+import 'screens/settings_screen.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+
+    SharedPreferences.getInstance().then((prefs) {
+      var darkModeOn = prefs.getBool('darkMode') ?? true;
+      runApp(
+        ChangeNotifierProvider<ThemeNotifier>(
+          builder: (_) => ThemeNotifier(darkModeOn ? lightTheme : darkTheme),
+          child: MyApp(),
+        ),
+      );
+    });
+
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'DYPIU Assist',
-
+      theme: themeNotifier.getTheme(),
       home: Home(),
       initialRoute: '/',
       routes: {
@@ -39,6 +58,7 @@ class MyApp extends StatelessWidget {
         AboutScreen.routeName: (ctx) => AboutScreen(),
         ReferenceBooks.routeName:(ctx) => ReferenceBooks(),
         ReferenceBooks2.routeName:(ctx) => ReferenceBooks2(),
+        SettingsPage.routeName : (ctx) => SettingsPage(),
 
       },
         onUnknownRoute: (settings) {
